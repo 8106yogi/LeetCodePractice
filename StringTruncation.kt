30590deeeb1e9aef813fc7212e1794c125f82e8e
Yogesh Chaudhary <8106yogi@gmail.com>
2026-01-18 22:53:02 -0600

    Kotlin String Truncation and Array Processing Logic

---
+++ StringTruncation.kt
@@ -0,0 +1,47 @@
+//Kotlin String Truncation and Array Processing Logic
+//Output
+//hello...
+//rashmi...
+//what is...
+//LeetCode...
+//Given an...
+//compress...
+//Begin with...
+//For each...
+//[hello..., yogesh , rashmi..., what is..., A ,  , LeetCode..., Given an..., compress..., Begin with..., For each..., test ]
+
+fun main() {
+val stringArray = arrayOf("hello myname is anthoni","yogesh", "rashmi chaudhary", "what is going on", "A", " ", "LeetCode https://leetcode.com › discuss › kotlin-solution",
+"Given an array of characters chars" , "compress it using the following algorithm:" ,"Begin with an empty string s",
+                          "For each group of consecutive repeating","test" )
+  val list=mutableListOf<String>()
+    for(s in stringArray){
+	val t= truncate(s,10)
+    if(t.isNotEmpty()){
+        list.add(t)
+    }
+    }
+    println(list)
+}
+
+
+
+fun truncate(s:String,k:Int):String{
+    val trimS=s.trim()
+	val strArray = trimS.split(" ")
+    val sb=StringBuilder()
+	for(str in strArray){
+        if(sb.length+str.length <=k ){
+            sb.append(str)
+            sb.append(" ")
+        }else{
+        	if(sb.isNotEmpty()){
+            	sb.deleteCharAt(sb.length-1) //remove extra space before adding ...
+	    		sb.append("...")
+			    println(sb.toString())
+          	}
+            break;
+        }
+    }	 
+return sb.toString()
+}

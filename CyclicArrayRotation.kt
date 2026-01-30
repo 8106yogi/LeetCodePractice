cb69b289ff9b6766a3816c47021229dbb283236d
Yogesh Chaudhary <8106yogi@gmail.com>
2026-01-29 20:10:04 -0600

    Add examples for cyclic array rotation in Kotlin
    Added example inputs and outputs for the cyclic array rotation algorithm.

---
+++ CyclicArrayRotation.kt
@@ -0,0 +1,52 @@
+//Cyclic Array Rotation Algorithm in Kotlin
+//input: 3,8,9,7,6
+//K = 3
+//output: 9,7,6,3,8
+
+//input: 1,2,3,4,5
+//K = 1
+//output: 5,1,2,3,4
+
+//input: 1,2,3,4
+//K = 2
+//output: 3,4,1,2
+
+//input: 1,2,3
+//K = 3
+//output: 1,2,3
+
+//input: 10
+//K = 5
+//output: 10
+
+fun solution(A: IntArray, K: Int): IntArray {
+
+if(A.size==0||A.size==1||A.size==K)return A
+
+var k=K
+
+k= K%A.size
+
+val arr=IntArray(A.size)
+
+    for(i in A.indices){
+
+        var j=0
+
+        if(i+k<A.size){
+
+            j = i + k
+
+        }else{
+
+             j= i+k - A.size
+
+        }
+
+        arr[j]= A[i]
+
+    }
+
+return arr
+
+}

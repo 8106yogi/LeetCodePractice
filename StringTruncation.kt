60967da5884c244cd04fb96d6eefb97d44ee6a27
Yogesh Chaudhary <yogesh.chaudhary.dev@gmail.com>
2026-02-11 18:33:21 -0600

    Update StringTruncation.kt

--- StringTruncation.kt
+++ StringTruncation.kt
@@ -1,30 +1,21 @@
 //Kotlin String Truncation and Array Processing Logic
-//Output
-//hello...
-//rashmi...
-//what is...
-//LeetCode...
-//Given an...
-//compress...
-//Begin with...
-//For each...
-//[hello..., yogesh , rashmi..., what is..., A ,  , LeetCode..., Given an..., compress..., Begin with..., For each..., test ]
+//Input: ["hello myname is anthoni","yogesh", "rashmi chaudhary", "what is going on", "A", " ", "LeetCode https://leetcode.com › discuss › kotlin-solution","Given an array of characters chars" , 
+//"compress it using the following algorithm:" ,"Begin with an empty string s", "For each group of consecutive repeating","test"]
+
+//Output: [hello..., yogesh , rashmi..., what is..., A ,  , LeetCode..., Given an..., compress..., Begin with..., For each..., test ]
 
 fun main() {
-val stringArray = arrayOf("hello myname is anthoni","yogesh", "rashmi chaudhary", "what is going on", "A", " ", "LeetCode https://leetcode.com › discuss › kotlin-solution",
-"Given an array of characters chars" , "compress it using the following algorithm:" ,"Begin with an empty string s",
-                          "For each group of consecutive repeating","test" )
-  val list=mutableListOf<String>()
+	val stringArray = arrayOf("hello myname is anthoni","yogesh", "rashmi chaudhary", "what is going on", "A", " ", "LeetCode https://leetcode.com › discuss › kotlin-solution","Given an array of characters chars",
+						  "compress it using the following algorithm:" ,"Begin with an empty string s", "For each group of consecutive repeating","test")
+	val list=mutableListOf<String>()
     for(s in stringArray){
-	val t= truncate(s,10)
-    if(t.isNotEmpty()){
-        list.add(t)
-    }
+		val t= truncate(s,10)
+    	if(t.isNotEmpty()){
+        	list.add(t)
+	    }
     }
     println(list)
 }
-
-
 
 fun truncate(s:String,k:Int):String{
     val trimS=s.trim()

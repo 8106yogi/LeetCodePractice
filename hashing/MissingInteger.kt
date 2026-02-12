f5ce16ded188b3a0eee3b77f6fad24e99a185a60
Yogesh Chaudhary <yogesh.chaudhary.dev@gmail.com>
2026-02-12 00:24:18 -0600

    Create MissingInteger.kt

---
+++ hashing/MissingInteger.kt
@@ -0,0 +1,24 @@
+//1. MissingInteger
+//Find the smallest positive integer that does not occur in a given sequence.
+//Write a function: fun solution(A: IntArray): Int
+//that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+//For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+//Given A = [1, 2, 3], the function should return 4.
+//Given A = [−1, −3], the function should return 1.
+//Write an efficient algorithm for the following assumptions:
+//N is an integer within the range [1..100,000];
+//each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+fun solution(A: IntArray): Int {
+    // Implement your solution here
+    val set =  A.toSet()
+    for(n in 1..set.size){
+        if(!set.contains(n)){
+        return n
+        }
+    }
+    return set.size+1
+}
+
+//Detected time complexity:
+//O(N) or O(N * log(N))

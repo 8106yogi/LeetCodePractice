9b0952373a10762e72ec45dfa8f6d50649b0630d
Yogesh Chaudhary <yogesh.chaudhary.dev@gmail.com>
2026-02-13 02:03:03 -0600

    Update NearbyDuplicateDetection.kt

--- NearbyDuplicateDetection.kt
+++ NearbyDuplicateDetection.kt
@@ -1,28 +1,16 @@
 //Kotlin Hash Map Approach for Nearby Duplicate Detection
+//219. Contains Duplicate II
 //Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
-
- 
-
-//Example 1:
-//Input: nums = [1,2,3,1], k = 3
-//Output: true
-
-//Example 2:
-//Input: nums = [1,0,1,1], k = 1
-//Output: true
-
-//Example 3:
-//Input: nums = [1,2,3,1,2,3], k = 2
-//Output: false
-
+//Example 1:  Input: nums = [1,2,3,1], k = 3  Output: true
+//Example 2: Input: nums = [1,0,1,1], k = 1   Output: true
+//Example 3: Input: nums = [1,2,3,1,2,3], k = 2   Output: false
 
 class Solution {
-
     fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
-        val map= mutableMapOf<Int,Int>()
+        val map= mutableMapOf<Int, Int>()
         for(i in nums.indices){
-            if(map[nums[i]]!=null){
-                if ( abs(i - (map[nums[i]]?:0)) <= k ) return true
+            if(map[nums[i]]!=null &&  Math.abs( (map[nums[i]]?:0)-i )<=k){
+                return true                
             }
             map[nums[i]] = i
         }
@@ -30,5 +18,15 @@ class Solution {
     }
 }
 
-
-
+//class Solution {
+//    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+//        val map= mutableMapOf<Int, Int>()
+//        for(i in nums.indices){
+//            if(map[nums[i]]!=null && Math.abs((map[nums[i]]?:0)-i)<=k){
+//                return true                
+//            }
+//            map[nums[i]] = i
+//        }
+//        return false
+//    }
+//}

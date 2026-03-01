35c59e1b1dbe975e8a248ac8a7f3598e68aa0722
Yogesh Chaudhary <yogesh.chaudhary.dev@gmail.com>
2026-02-28 22:22:40 -0600

    Add solution for triangular triplet problem
    Implement a function to check for triangular triplets in an array.

---
+++ Codility_Sorting_Triangle.kt
@@ -0,0 +1,29 @@
+//Triangle Task description ; An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+//A[P] + A[Q] > A[R], A[Q] + A[R] > A[P], A[R] + A[P] > A[Q].
+//For example, consider array A such that:
+//  A[0] = 10    A[1] = 2    A[2] = 5  A[3] = 1     A[4] = 8    A[5] = 20  ; Triplet (0, 2, 4) is triangular.
+//Write a function: that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+//For example, given array A such that:   A[0] = 10    A[1] = 2    A[2] = 5 ;   A[3] = 1     A[4] = 8    A[5] = 20
+//the function should return 1, as explained above. Given array A such that:
+//  A[0] = 10    A[1] = 50    A[2] = 5
+//  A[3] = 1 the function should return 0. 
+//Constraints ; N is an integer within the range [0..100,000]; //each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+
+fun solution(A: IntArray): Int {
+    // Implement your solution here
+    for(i in A.indices){
+        for(j in i+1..A.size-1){
+            for(k in j+1..A.size-1){
+                if(isTrinangle(A[i],A[j],A[k])) return 1
+
+            }
+        }
+    }
+return 0
+}
+
+fun isTrinangle(p:Int,q:Int, r:Int):Boolean{
+    if(p+q>r && q+r>p && r+p>q) return true
+    else return false
+}
